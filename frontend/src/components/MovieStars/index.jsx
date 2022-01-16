3de0bef2d0263676e56bae9f4ef192f529ee5301
Lucas Suzuki <lucasarsuzuki@gmail.com>
2022-01-16 14:55:10 -0300

    Navigation buttons

--- frontend/src/components/MovieStars/index.jsx
+++ frontend/src/components/MovieStars/index.jsx
@@ -1,6 +1,7 @@
 import { ReactComponent as StarFull } from 'assets/img/star-full.svg';
 import { ReactComponent as StarHalf } from 'assets/img/star-half.svg';
 import { ReactComponent as StarEmpty } from 'assets/img/star-empty.svg';
+import './styles.css';
 
 function MovieStars() {
 
